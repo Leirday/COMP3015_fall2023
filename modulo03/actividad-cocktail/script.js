e3cb4bce56c7d015e8c7220806cb6d20c7a68302
Lrdy <63563648+Leirday@users.noreply.github.com>
2023-12-06 22:24:29 -0400

    Add files via upload

---
+++ modulo03/actividad-cocktail/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $("#btnDrink").click(function() {
+        $.get("https://www.thecocktaildb.com/api/json/v1/1/random.php", 
+            function(data) {
+                const drink = data.drinks[0];
+                $('#tituloH1').html( drink.strDrink ); // titulo
+        });
+    });
+});
